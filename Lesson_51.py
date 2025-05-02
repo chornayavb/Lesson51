ed174c8870013242d056ca0f06226a06cc1fa45f
Sasha <chornayavb@gmail.com>
2025-05-02 21:05:09 +0300

    second commit

--- Lesson_51.py
+++ Lesson_51.py
@@ -5,5 +5,5 @@ print('Hello World')
 '''
 git branch - проверить
 git checkout name - переключить
-git branch -b name - создать
-git branch -D name - удалить'''
+git checkout -b name - создать
+git checkout -D name - удалить'''
