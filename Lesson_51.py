5aafeb2e3ede378f633392569fdab3b33fb6d5de
Sasha <chornayavb@gmail.com>
2025-05-02 20:17:23 +0300

    first commit

---
+++ Lesson_51.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print('Hello World')
+print('Hello World')
+
+'''
+git branch - проверить
+git checkout name - переключить
+git branch -b name - создать
+git branch -D name - удалить'''
